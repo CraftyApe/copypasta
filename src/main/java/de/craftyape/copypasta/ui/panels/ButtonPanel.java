dc6fb598c56eda19d6ef5bf5a8b902ba5cd27566
MadMonkeh <70608370+CraftyApe@users.noreply.github.com>
2024-03-31 15:04:28 +0200

    Revert "implemented clipboard logic"
    This reverts commit f963c1db56d3ca8d9033088f684a03a96d363c29.

--- src/main/java/de/craftyape/copypasta/ui/panels/ButtonPanel.java
+++ src/main/java/de/craftyape/copypasta/ui/panels/ButtonPanel.java
@@ -4,36 +4,31 @@ import de.craftyape.copypasta.entities.Pasta;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 
 public class ButtonPanel extends ParentPanel {
 
-    private Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+public ButtonPanel(Pasta[] pastas) {
+    this.setLayout(new GridLayout(5, 5));
+    this.pastas = pastas;
+    initComponents();
+}
 
-    public ButtonPanel(Pasta[] pastas) {
-        this.setLayout(new GridLayout(5, 5));
-        this.pastas = pastas;
-        initComponents();
+private void initComponents() {
+
+    Dimension buttonSize = new Dimension(240, 160);
+
+    for (Pasta pasta : pastas) {
+        JButton jButton = new JButton(pasta.getTitle());
+        jButton.setFont(fontPlain24);
+        jButton.addActionListener(e -> buttonClicked(pasta));
+        jButton.setPreferredSize(buttonSize);
+        setConstraints(pasta.getPosX(), pasta.getPosY());
+        add(jButton, gridBagConstraints);
     }
+}
 
-    private void initComponents() {
-
-        Dimension buttonSize = new Dimension(240, 160);
-
-        for (Pasta pasta : pastas) {
-            JButton jButton = new JButton(pasta.getTitle());
-            jButton.setFont(fontPlain24);
-            jButton.addActionListener(e -> buttonClicked(pasta));
-            jButton.setPreferredSize(buttonSize);
-            setConstraints(pasta.getPosX(), pasta.getPosY());
-            add(jButton, gridBagConstraints);
-        }
-    }
-
-    private void buttonClicked(Pasta pasta) {
-        clipboard.setContents(new StringSelection(pasta.getText()), null);
-        LOG.info("{} - position: ({}, {})", pasta.getText(), pasta.getPosX(), pasta.getPosY());
-    }
+private void buttonClicked(Pasta pasta) {
+    LOG.info("{} - position: ({}, {})", pasta.getText(), pasta.getPosX(), pasta.getPosY());
+}
 
 }
